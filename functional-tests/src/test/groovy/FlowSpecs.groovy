8db582f7fa247d04127713325fad576bba975164
georgelwalker <gwalker@escapesystems.com>
2017-06-05 15:54:22 -0700

    Updated functional test

--- functional-tests/src/test/groovy/FlowSpecs.groovy
+++ functional-tests/src/test/groovy/FlowSpecs.groovy
@@ -14,43 +14,4 @@ class FlowSpecs extends GebReportingSpec {
 		then: "Still at Info Page"
 		at QSystemInfoPage
 	}
-
-
-    def "Login"(){
-		given:
-		to QUserPage
-		
-		when:
-		
-		Thread.Sleep(3000.longValue())
-		$("input", 0).value = "English"
-		Thread.Sleep(3000.longValue())
-        $("input", 1).value = "Administrator"
-        $("button", 0).click()
-		
-		then:
-		assert ($("span", text: "Administrator").text == "Administrator")
-	  }
-
-	  
-/*
-dPwPn
-    @Unroll
-    def "Navigate Page from: #startPage, click Link: #clickLink, Assert Page: #assertPage"(){
-        when:
-        to startPage
-
-        and:
-        (1..clickCount).each{
-            $("a", id:"$clickLink").click()
-        }
-
-        then:
-        at assertPage
-
-        where:
-        startPage           | clickLink                     | clickCount    | timeoutSeconds    || assertPage
-        QUserPagePage       | "navbar-notifications"        | 1             | 3                 || NotificationsPage
-    }
-	*/
 }
