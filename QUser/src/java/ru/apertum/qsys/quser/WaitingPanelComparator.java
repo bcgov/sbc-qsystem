e33fb0ec381b3f8e929a0619e89bfbd92563af37
IDIR\jassingh <jasmeet17mast@gmail.com>
2017-06-12 15:20:03 -0700

    Add Sorting Support on Waiting panel columns

---
+++ QUser/src/java/ru/apertum/qsys/quser/WaitingPanelComparator.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.apertum.qsys.quser;
+
+import java.io.Serializable;
+import java.util.Comparator;
+
+import ru.apertum.qsystem.common.model.QCustomer;
+
+/**
+ *
+ * @author jassingh
+ */
+public class WaitingPanelComparator implements Comparator<Object>, Serializable{
+    private  static  final long serialVersionUID = -2127053833562854322L;
+    
+    private boolean asc = true;
+    private int type = 0;
+ 
+    public WaitingPanelComparator(boolean asc, int type) {
+        this.asc = asc;
+        this.type = type;
+    }
+ 
+    public int getType() {
+        return type;
+    }
+ 
+    public void setType(int type) {
+        this.type = type;
+    }
+    
+    @Override
+    public int compare(Object o1, Object o2) {
+        QCustomer customer1 = (QCustomer) o1;
+        QCustomer customer2 = (QCustomer) o2;
+        switch (type) {
+        case 1: // Ticket Time
+            return customer1.standTimeinHHMMSS().compareTo(customer2.standTimeinHHMMSS()) * (asc ? 1 : -1);
+        case 2: // Ticket Number
+            return customer1.getFullNumber().compareTo(customer2.getFullNumber()) * (asc ? 1 : -1);
+        case 3: // Service Name
+            return customer1.getService().getName().compareTo(customer2.getService().getName()) * (asc ? 1 : -1);
+        case 4: // Priority
+            return customer1.taskPriority().compareTo(customer2.taskPriority()) * (asc ? 1 : -1);
+        case 5: // State In
+            return customer1.currentStateIn().compareTo(customer2.currentStateIn()) * (asc ? 1 : -1);
+        default: // Ticket Number //
+            return customer1.standTimeinHHMMSS().compareTo(customer2.standTimeinHHMMSS()) * (asc ? 1 : -1);
+        }
+ 
+    }
+    
+}
