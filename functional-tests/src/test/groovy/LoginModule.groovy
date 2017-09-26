e82405476dcc1c8c23f979f77301ad310d48283b
Mark Walle <mwalle@gmail.com>
2017-09-26 15:15:29 -0700

    testing

--- functional-tests/src/test/groovy/LoginModule.groovy
+++ functional-tests/src/test/groovy/LoginModule.groovy
@@ -10,29 +10,24 @@ import spock.lang.Unroll
  
 class LoginModule extends Module { 
 
-    void userLogin(){ 
-
-        def usr = '#' + browser.driver.executeScript("zk.Widget.\$('$usr').uuid") + '-real'
-        def pwd = browser.driver.executeScript("zk.Widget.\$('$pwd').uuid")
+    void userLogin(){
 
         given: "I start on the home page" 
             to QUserPage 
         when: "My credentials are input and login is clicked" 
             
-            // https://www.zkoss.org/wiki/ZK_Client-side_Reference/General_Control/UI_Composing#Find_a_Widget_at_Client 
-            // https://www.zkoss.org/javadoc/latest/jsdoc/zk/Widget.html#$n()
-            //retrieves the combobox by excecuting jquery selector and the zk client side widget functions
+            def user = js."zk.Widget.\$('\$usr').uuid" + "-real"
+            def password = js."zk.Widget.\$('\$pwd').uuid"
 
-
-            $("input", id:usr).value("Mark")                    // look into static assignments of id attributes 
-            $("input", id:pwd).value("password")                // https://www.zkoss.org/wiki/ZK_Developer's_Reference/Testing/Testing_Tips 
+            $("input", id:user).value("mwalle")                    // look into static assignments of id attributes 
+            $("input", id:password).value("password")                // https://www.zkoss.org/wiki/ZK_Developer's_Reference/Testing/Testing_Tips 
             $("button","class":"login-button z-button").click() // THIS ui-sref="authentication.signin" needs to be implemented in the .zul source 
-         
+
         then: "The home page refreshes" 
             at QUserPage
-            waitFor { $("span", "class":"login-text z-label")[0].text() != "" } 
+            waitFor { $("span", "class":"login-text z-label")[2].text() != "" } 
          
         expect: "I am logged in" 
-        assert { $("span", "class":"login-text z-label")[0].text() == "Mark" } 
+            assert { $("span", "class":"login-text z-label")[2].text() == "mwalle" } 
     } 
 }
