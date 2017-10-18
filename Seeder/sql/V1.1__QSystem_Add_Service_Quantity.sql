4b5dbfd078b084f4de554ef706ad6abad9daed1f
Karim Gillani <karim.gillani@gov.bc.ca>
2017-10-18 11:33:48 -0700

    Resolve Issue #184: Database update not updating all database instances

--- Seeder/sql/V1.1__QSystem_Add_Service_Quantity.sql
+++ Seeder/sql/V1.1__QSystem_Add_Service_Quantity.sql
@@ -1,13 +1,12 @@
-ALTER TABLE `clients` 
-ADD COLUMN `Service_quantity` VARCHAR(45) NOT NULL DEFAULT '1';
+ALTER TABLE `clients`
+ADD COLUMN `service_quantity` VARCHAR(45) NOT NULL DEFAULT '1';
 
-ALTER TABLE `statistic` 
-ADD COLUMN `service_quantity` INT(11) NOT NULL;
-
-DROP TRIGGER IF EXISTS `qsystem`.`insert_to_statistic`;
+ALTER TABLE `statistic`
+ADD COLUMN `service_quantity` VARCHAR(45) NOT NULL;
 
 DELIMITER $$
-USE `qsystem`$$
+DROP TRIGGER IF EXISTS `insert_to_statistic`$$
+
 CREATE DEFINER=`root`@`%` TRIGGER insert_to_statistic
     AFTER INSERT ON clients
     FOR EACH ROW
@@ -15,9 +14,9 @@ BEGIN
     SET @finish_start= TIMEDIFF(NEW.finish_time, NEW.start_time);
     SET @start_starnd = TIMEDIFF(NEW.start_time, NEW.stand_time);
     INSERT
-        INTO statistic(state_in, results_id, user_id, client_id, service_id, user_start_time, user_finish_time, client_stand_time, user_work_period, client_wait_period, client_welcome_time, client_invite_time, service_quantity) 
+        INTO statistic(state_in, results_id, user_id, client_id, service_id, user_start_time, user_finish_time, client_stand_time, user_work_period, client_wait_period, client_welcome_time, client_invite_time, service_quantity)
     VALUES
-        (NEW.state_in, NEW.result_id, NEW.user_id, NEW.id, NEW.service_id, NEW.start_time, NEW.finish_time, NEW.stand_time, 
+        (NEW.state_in, NEW.result_id, NEW.user_id, NEW.id, NEW.service_id, NEW.start_time, NEW.finish_time, NEW.stand_time,
         round(
                 (HOUR(@finish_start) * 60 * 60 +
                  MINUTE(@finish_start) * 60 +
@@ -25,14 +24,14 @@ BEGIN
         round(
                 (HOUR(@start_starnd) * 60 * 60 +
                 MINUTE(@start_starnd) * 60 +
-                SECOND(@start_starnd) + 59)/60)  
+                SECOND(@start_starnd) + 59)/60)
         , NEW.welcome_time, New.invite_time, New.service_quantity);
 END$$
 DELIMITER ;
-DROP TRIGGER IF EXISTS `qsystem`.`update_to_statistic`;
 
 DELIMITER $$
-USE `qsystem`$$
+DROP TRIGGER IF EXISTS `update_to_statistic`$$
+
 CREATE DEFINER=`root`@`%` TRIGGER update_to_statistic
     AFTER UPDATE ON clients
     FOR EACH ROW
@@ -40,9 +39,9 @@ BEGIN
     SET @finish_start= TIMEDIFF(NEW.finish_time, NEW.start_time);
     SET @start_starnd = TIMEDIFF(NEW.start_time, NEW.stand_time);
     INSERT
-        INTO statistic(state_in, results_id, user_id, client_id, service_id, user_start_time, user_finish_time, client_stand_time, user_work_period, client_wait_period, client_welcome_time, client_invite_time, service_quantity) 
+        INTO statistic(state_in, results_id, user_id, client_id, service_id, user_start_time, user_finish_time, client_stand_time, user_work_period, client_wait_period, client_welcome_time, client_invite_time, service_quantity)
     VALUES
-        (NEW.state_in, NEW.result_id, NEW.user_id, NEW.id, NEW.service_id, NEW.start_time, NEW.finish_time, NEW.stand_time, 
+        (NEW.state_in, NEW.result_id, NEW.user_id, NEW.id, NEW.service_id, NEW.start_time, NEW.finish_time, NEW.stand_time,
         round(
                 (HOUR(@finish_start) * 60 * 60 +
                  MINUTE(@finish_start) * 60 +
