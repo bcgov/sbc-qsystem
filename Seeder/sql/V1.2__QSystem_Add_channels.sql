38704d58ee94e9b208b5314bff6f64ab21f5b807
Xu <ANDREWXU@BCGOV>
2017-10-20 16:14:02 -0700

    update v1.2.sql to add Channels, and add channels for nonrecep mode

---
+++ Seeder/sql/V1.2__QSystem_Add_channels.sql
@@ -0,0 +1,56 @@
+ALTER TABLE `clients`
+ADD COLUMN `channels` BIGINT(20) NOT NULL DEFAULT '1';
+
+ALTER TABLE `statistic`
+ADD COLUMN `channels` BIGINT(20) NOT NULL;
+
+
+DELIMITER $$
+DROP TRIGGER IF EXISTS `insert_to_statistic`$$
+
+CREATE DEFINER=`root`@`%` TRIGGER insert_to_statistic
+    AFTER INSERT ON clients
+    FOR EACH ROW
+BEGIN
+    SET @finish_start= TIMEDIFF(NEW.finish_time, NEW.start_time);
+    SET @start_starnd = TIMEDIFF(NEW.start_time, NEW.stand_time);
+    INSERT
+        INTO statistic(state_in, results_id, user_id, client_id, service_id, user_start_time, user_finish_time, client_stand_time, user_work_period, client_wait_period, client_welcome_time, client_invite_time, service_quantity, channels)
+    VALUES
+        (NEW.state_in, NEW.result_id, NEW.user_id, NEW.id, NEW.service_id, NEW.start_time, NEW.finish_time, NEW.stand_time,
+        round(
+                (HOUR(@finish_start) * 60 * 60 +
+                 MINUTE(@finish_start) * 60 +
+                 SECOND(@finish_start) + 59)/60),
+        round(
+                (HOUR(@start_starnd) * 60 * 60 +
+                MINUTE(@start_starnd) * 60 +
+                SECOND(@start_starnd) + 59)/60)
+        , NEW.welcome_time, New.invite_time, New.service_quantity, New.channels);
+END$$
+DELIMITER ;
+
+DELIMITER $$
+DROP TRIGGER IF EXISTS `update_to_statistic`$$
+
+CREATE DEFINER=`root`@`%` TRIGGER update_to_statistic
+    AFTER UPDATE ON clients
+    FOR EACH ROW
+BEGIN
+    SET @finish_start= TIMEDIFF(NEW.finish_time, NEW.start_time);
+    SET @start_starnd = TIMEDIFF(NEW.start_time, NEW.stand_time);
+    INSERT
+        INTO statistic(state_in, results_id, user_id, client_id, service_id, user_start_time, user_finish_time, client_stand_time, user_work_period, client_wait_period, client_welcome_time, client_invite_time, service_quantity, channels)
+    VALUES
+        (NEW.state_in, NEW.result_id, NEW.user_id, NEW.id, NEW.service_id, NEW.start_time, NEW.finish_time, NEW.stand_time,
+        round(
+                (HOUR(@finish_start) * 60 * 60 +
+                 MINUTE(@finish_start) * 60 +
+                 SECOND(@finish_start) + 59)/60),
+        round(
+                (HOUR(@start_starnd) * 60 * 60 +
+                MINUTE(@start_starnd) * 60 +
+                SECOND(@start_starnd) + 59)/60)  
+        , NEW.welcome_time, New.invite_time, New.service_quantity, New.channels);
+END$$
+DELIMITER ;
