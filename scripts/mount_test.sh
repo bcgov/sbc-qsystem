8973a086b5eacb15a528bc33b4ba23dcb9b9cd4b
Mark Walle <mwalle@gmail.com>
2017-11-02 14:02:03 -0700

    mount_test.sh checks path to directory (first arg) exists and is reached before timeout (second arg)

---
+++ scripts/mount_test.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+echo "Starting mount test."
+
+mount_point=$1
+mount_timeout=$2
+
+echo "Checking: $mount_point for $mount_timeout seconds."
+
+# check directory until reached or timeout
+for (( i=1 ; i<$mount_timeout ; i++ )) ; do
+  if [ ! -d "$mount_point" ]; then
+    sleep 1
+  else
+    break
+  fi
+done
+
+# final mount test
+if [ ! -d "$mount_point" ]; then
+  echo "Could not list ${mount_point} in $mount_timeout sec."
+  curl -X POST --data-urlencode "payload={\"channel\": \"#sbc-cfms\", \"username\": \"mountbot\", \"text\": \"Liveness Probe failed on ${HOSTNAME}: could not reach ${mount_point} after $mount_timeout seconds.\", \"icon_emoji\": \":sleuth_or_spy:\"}" $SLACKHOOK
+  exit 1
+else
+  curl -X POST --data-urlencode "payload={\"channel\": \"#sbc-cfms\", \"username\": \"mountbot\", \"text\": \"Liveness Probe succeeded on ${HOSTNAME}: ${mount_point} exists and is reachable.\", \"icon_emoji\": \":sleuth_or_spy:\"}" $SLACKHOOK
+    echo "File test write success"
+    exit 0
+  fi
+fi
