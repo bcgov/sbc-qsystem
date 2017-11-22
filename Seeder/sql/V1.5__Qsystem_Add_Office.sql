5a063e6e7c11f3893afab0b8350502f26e86aa6f
Karim Gillani <karim.gillani@gov.bc.ca>
2017-11-21 18:27:08 -0800

    Update V1.5
    Remove reference to single schema

--- Seeder/sql/V1.5__Qsystem_Add_Office.sql
+++ Seeder/sql/V1.5__Qsystem_Add_Office.sql
@@ -1,70 +1,70 @@
-CREATE TABLE IF NOT EXISTS `qsystem`.`offices` (
+CREATE TABLE IF NOT EXISTS `offices` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `smartboard_type` VARCHAR(45) NOT NULL DEFAULT 'callbyticket',
   `deleted` DATETIME
   PRIMARY KEY (`id`));
 
-INSERT INTO `qsystem`.`offices` (name)    
+INSERT INTO `offices` (name)    
 select 'Test Office' from dual   
-WHERE NOT EXISTS (SELECT 1 FROM `qsystem`.`offices` where name = 'Test Office');
+WHERE NOT EXISTS (SELECT 1 FROM `offices` where name = 'Test Office');
 
-ALTER TABLE `qsystem`.`users` ADD COLUMN office_id int(11) NOT NULL DEFAULT 1;
-ALTER TABLE `qsystem`.`users` 
+ALTER TABLE `users` ADD COLUMN office_id int(11) NOT NULL DEFAULT 1;
+ALTER TABLE `users` 
 ADD INDEX `fk_users_office_idx` (`office_id` ASC);
-ALTER TABLE `qsystem`.`users` 
+ALTER TABLE `users` 
 ADD CONSTRAINT `fk_users_office`
   FOREIGN KEY (`office_id`)
-  REFERENCES `qsystem`.`offices` (`id`)
+  REFERENCES 'offices` (`id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
 
-ALTER TABLE `qsystem`.`clients`
+ALTER TABLE `clients`
 MODIFY COLUMN stand_time DATETIME NULL;
 
-ALTER TABLE `qsystem`.`clients`
+ALTER TABLE `clients`
 MODIFY COLUMN start_time DATETIME NULL;
 
-ALTER TABLE `qsystem`.`clients`
+ALTER TABLE `clients`
 MODIFY COLUMN finish_time DATETIME NULL;
 
-ALTER TABLE `qsystem`.`clients`
+ALTER TABLE `clients`
 MODIFY COLUMN welcome_time DATETIME NULL;
 
-ALTER TABLE `qsystem`.`clients`
+ALTER TABLE `clients`
 MODIFY COLUMN invite_time DATETIME NULL;
 
-ALTER TABLE `qsystem`.`clients`
+ALTER TABLE `clients`
 MODIFY COLUMN client_stand_time DATETIME NULL;
 
-ALTER TABLE `qsystem`.`statistic`
+ALTER TABLE `statistic`
 MODIFY COLUMN user_start_time DATETIME NULL;
 
-ALTER TABLE `qsystem`.`statistic`
+ALTER TABLE `statistic`
 MODIFY COLUMN user_finish_time DATETIME NULL;
 
-ALTER TABLE `qsystem`.`statistic`
+ALTER TABLE 'statistic`
 MODIFY COLUMN client_welcome_time DATETIME NULL;
 
-ALTER TABLE `qsystem`.`statistic`
+ALTER TABLE `statistic`
 MODIFY COLUMN client_invite_time DATETIME NULL;
 
-ALTER TABLE `qsystem`.`statistic`
+ALTER TABLE `statistic`
 MODIFY COLUMN user_work_period DATETIME NULL;
 
-ALTER TABLE `qsystem`.`statistic`
+ALTER TABLE `statistic`
 MODIFY COLUMN client_wait_period DATETIME NULL;
 
-ALTER TABLE `qsystem`.`statistic`
+ALTER TABLE `statistic`
 ADD COLUMN office_id int(11) NOT NULL DEFAULT 1;
 
-ALTER TABLE `qsystem`.`clients` 
+ALTER TABLE `clients` 
 ADD INDEX `fk_clients_office_idx` (`office_id` ASC);
 
-ALTER TABLE `qsystem`.`clients` 
+ALTER TABLE `clients` 
 ADD CONSTRAINT `fk_clients_office`
   FOREIGN KEY (`office_id`)
-  REFERENCES `qsystem`.`offices` (`id`)
+  REFERENCES `offices` (`id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
 
@@ -79,6 +79,6 @@ CREATE TABLE `services_offices` (
   CONSTRAINT `fk_service_office_office_id` FOREIGN KEY (`office_id`) REFERENCES `offices` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-ALTER TABLE `qsystem`.`services` ADD COLUMN smartboard_yn char(1) NOT NULL DEFAULT 'Y';
+ALTER TABLE `services` ADD COLUMN smartboard_yn char(1) NOT NULL DEFAULT 'Y';
 
-ALTER TABLE `qsystem`.`clients` ADD COLUMN comments varchar(2500);
+ALTER TABLE `clients` ADD COLUMN comments varchar(2500);
