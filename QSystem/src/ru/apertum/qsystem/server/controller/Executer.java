be9d8b038c876e7cbb111df9c50be5e32bc11852
Chris <Chris.McIntosh@gov.bc.ca>
2018-04-18 15:05:24 -0700

    Put sample Snowplow code BACK IN Executer.  Code now seems to work

--- QSystem/src/ru/apertum/qsystem/server/controller/Executer.java
+++ QSystem/src/ru/apertum/qsystem/server/controller/Executer.java
@@ -240,65 +240,70 @@ public final class Executer {
         // get the client adapter
         // this is used by the Java tracker to transmit events to the collector
         HttpClientAdapter okHttpClientAdapter = getClient(collectorEndpoint);
-        
+
         Emitter emitter = SimpleEmitter.builder()
-                .httpClientAdapter( okHttpClientAdapter ) // Required
+                .httpClientAdapter(okHttpClientAdapter) // Required
                 .threadCount(20) // Default is 50
                 .build();
-            
+
         Tracker tracker = new Tracker.TrackerBuilder(emitter, namespace, appID)
-                            .base64(baseSetting)
-                            .platform(DevicePlatform.Desktop)
-                            .build();
-        
+                .base64(baseSetting)
+                .platform(DevicePlatform.Desktop)
+                .build();
+
         //----------------------------------------
         // Create a Map of the data you want to include...
         Map<String, Object> citizenMap = new HashMap<>();
         citizenMap.put("client_id", 123456);
-        SelfDescribingJson citizen = new SelfDescribingJson("iglu:ca.bc.gov.cfmspoc/citizen/jsonschema/1-0-0", citizenMap);
-    
+        SelfDescribingJson citizen = new SelfDescribingJson(
+                "iglu:ca.bc.gov.cfmspoc/citizen/jsonschema/1-0-0", citizenMap);
+
         //----------------------------------------
         Map<String, Object> officeMap = new HashMap<>();
         officeMap.put("office_id", 12);
         officeMap.put("office_type", "reception");
-        SelfDescribingJson office = new SelfDescribingJson("iglu:ca.bc.gov.cfmspoc/office/jsonschema/1-0-0", officeMap);
-        
+        SelfDescribingJson office = new SelfDescribingJson(
+                "iglu:ca.bc.gov.cfmspoc/office/jsonschema/1-0-0", officeMap);
+
         //----------------------------------------
         Map<String, Object> agentMap = new HashMap<>();
         agentMap.put("agent_id", 12);
         agentMap.put("role", "CSR");
-        SelfDescribingJson agent = new SelfDescribingJson("iglu:ca.bc.gov.cfmspoc/agent/jsonschema/1-0-0", agentMap);
-        
+        SelfDescribingJson agent = new SelfDescribingJson(
+                "iglu:ca.bc.gov.cfmspoc/agent/jsonschema/1-0-0", agentMap);
+
         //----------------------------------------
         List<SelfDescribingJson> contexts = new ArrayList<>();
         contexts.add(citizen);
         contexts.add(office);
         contexts.add(agent);
-        
+
         // Create your event data -- in this example the event has data of its own
         Map<String, Object> chooseserviceMap = new HashMap<>();
-        chooseserviceMap.put("channel","in-person");
-        chooseserviceMap.put("program_id",45);
-        chooseserviceMap.put("parent_id",0);
-        chooseserviceMap.put("program_name","An amazing program");
-        chooseserviceMap.put("transaction_name","A fantastic transaction");
-        chooseserviceMap.put("quick_txn",false);
-    
-        SelfDescribingJson chooseserviceData = new SelfDescribingJson("iglu:ca.bc.gov.cfmspoc/chooseservice/jsonschema/1-0-0", chooseserviceMap);
+        chooseserviceMap.put("channel", "in-person");
+        chooseserviceMap.put("program_id", 45);
+        chooseserviceMap.put("parent_id", 0);
+        chooseserviceMap.put("program_name", "An amazing program");
+        chooseserviceMap.put("transaction_name", "A fantastic transaction");
+        chooseserviceMap.put("quick_txn", false);
+
+        SelfDescribingJson chooseserviceData = new SelfDescribingJson(
+                "iglu:ca.bc.gov.cfmspoc/chooseservice/jsonschema/1-0-0", chooseserviceMap);
         // Track your event with your custom event data
         tracker.track(Unstructured.builder()
-            .eventData(chooseserviceData)
-            .customContext(contexts)
-            .build());
-    
+                .eventData(chooseserviceData)
+                .customContext(contexts)
+                .build());
+
         //----------------------------------------
         // Create your event data -- in this example the event has no data of its own
-        SelfDescribingJson beginserviceData = new SelfDescribingJson("iglu:ca.bc.gov.cfmspoc/beginservice/jsonschema/1-0-0");
+        SelfDescribingJson beginserviceData = new SelfDescribingJson(
+                "iglu:ca.bc.gov.cfmspoc/beginservice/jsonschema/1-0-0");
         // Track your event with your custom event data
         tracker.track(Unstructured.builder()
-            .eventData(beginserviceData)
-            .customContext(contexts)
-            .build());
+                .eventData(beginserviceData)
+                .customContext(contexts)
+                .build());
     }
 
     //  CM:  ==>  End of Snowplow routine.
