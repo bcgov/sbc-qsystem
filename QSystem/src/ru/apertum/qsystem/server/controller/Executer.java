5607c950b1a1b1610bf3c7e180dd650f720afe69
Chris <Chris.McIntosh@gov.bc.ca>
2018-04-19 16:19:31 -0700

    Commented out Snowplow code, will be moved.

--- QSystem/src/ru/apertum/qsystem/server/controller/Executer.java
+++ QSystem/src/ru/apertum/qsystem/server/controller/Executer.java
@@ -232,7 +232,7 @@ public final class Executer {
 
     //  CM:  ==>  End of Snowplow variables.
 
-    //  CM:  ==>  Start of Snowplow routine.    
+    //  CM:  ==>  Start of Snowplow routine to get a client.    
     //========================================
     public static HttpClientAdapter getClient(String url) {
         // use okhttp to send events
@@ -247,6 +247,7 @@ public final class Executer {
                 .httpClient(client)
                 .build();
     }
+    //  CM:  ==>  End of Snowplow routine to get a client.    
 
     public void TestSnowplow(Long clientId, int officeId, int agentId) {
         // get the client adapter
@@ -326,6 +327,81 @@ public final class Executer {
     }
 
     //  CM:  ==>  End of Snowplow routine.
+
+    //  CM:  ==>  Start of Snowplow routine to choose a service.
+    public void SnowplowChooseService(CmdParams params) {
+
+        //        QLog.l().logger().debug("==> Snowplow: C: " + clientId + "; O: " + officeId + "; A: "
+        //                + agentId);
+
+        //        //  Need to get service from params.
+        //        final QService service = QServiceTree.getInstance().getById(params.serviceId);
+        //
+        //        QLog.l().logQUser().debug("==> Choosing a service from Params:");
+        //        QLog.l().logQUser().debug("    --> Channel:  " + params.channels);
+        //        QLog.l().logQUser().debug("    --> SvcId:    " + params.serviceId);
+        //        QLog.l().logQUser().debug("    --> SvcPId:   " + service.getParentId());
+        //        QLog.l().logQUser().debug("    --> SvcCat:   " + service.getParent().getName());
+        //        QLog.l().logQUser().debug("    --> SvtTrans: " + service.getName());
+        //        QLog.l().logQUser().debug("    --> Q.Txn:    " + (params.custQtxn ? "True"
+        //                : "False"));
+
+        //----------------------------------------
+        //        // Create a Map of the data you want to include...
+        //        Map<String, Object> citizenMap = new HashMap<>();
+        //        citizenMap.put("client_id", clientId);
+        //        SelfDescribingJson citizen = new SelfDescribingJson(
+        //                "iglu:ca.bc.gov.cfmspoc/citizen/jsonschema/1-0-0", citizenMap);
+        //
+        //        //----------------------------------------
+        //        Map<String, Object> officeMap = new HashMap<>();
+        //        officeMap.put("office_id", officeId);
+        //        officeMap.put("office_type", "reception");
+        //        SelfDescribingJson office = new SelfDescribingJson(
+        //                "iglu:ca.bc.gov.cfmspoc/office/jsonschema/1-0-0", officeMap);
+        //
+        //        //----------------------------------------
+        //        Map<String, Object> agentMap = new HashMap<>();
+        //        agentMap.put("agent_id", agentId);
+        //        agentMap.put("role", "CSR");
+        //        SelfDescribingJson agent = new SelfDescribingJson(
+        //                "iglu:ca.bc.gov.cfmspoc/agent/jsonschema/1-0-0", agentMap);
+        //
+        //        //----------------------------------------
+        //        List<SelfDescribingJson> contexts = new ArrayList<>();
+        //        contexts.add(citizen);
+        //        contexts.add(office);
+        //        contexts.add(agent);
+        //
+        //        // Create your event data -- in this example the event has data of its own
+        //        Map<String, Object> chooseserviceMap = new HashMap<>();
+        //        chooseserviceMap.put("channel", "in-person");
+        //        chooseserviceMap.put("program_id", 45);
+        //        chooseserviceMap.put("parent_id", 0);
+        //        chooseserviceMap.put("program_name", "An amazing program");
+        //        chooseserviceMap.put("transaction_name", "A fantastic transaction");
+        //        chooseserviceMap.put("quick_txn", false);
+        //
+        //        SelfDescribingJson chooseserviceData = new SelfDescribingJson(
+        //                "iglu:ca.bc.gov.cfmspoc/chooseservice/jsonschema/1-0-0", chooseserviceMap);
+        //        // Track your event with your custom event data
+        //        tracker.track(Unstructured.builder()
+        //                .eventData(chooseserviceData)
+        //                .customContext(contexts)
+        //                .build());
+        //
+        //        //----------------------------------------
+        //        // Create your event data -- in this example the event has no data of its own
+        //        SelfDescribingJson beginserviceData = new SelfDescribingJson(
+        //                "iglu:ca.bc.gov.cfmspoc/beginservice/jsonschema/1-0-0");
+        //        // Track your event with your custom event data
+        //        tracker.track(Unstructured.builder()
+        //                .eventData(beginserviceData)
+        //                .customContext(contexts)
+        //                .build());
+        QLog.l().logger().debug("    --> After last call");
+    }
+    //  CM:  ==>  End of Snowplow routine to choose a service.
 
     //  CM:  This variable sets the states in which a customer can be called.
     //  CM:  Used to prevent two CSRs calling the same customer at the same time.
@@ -3638,6 +3714,7 @@ public final class Executer {
                 customer.setTempQuickTxn(cmdParams.custQtxn);
                 customer.setIsInSequence(cmdParams.in_sequence);
                 customer.setLogWaitQueue(cmdParams.log_waitqueue);
+                customer.setCreateSnowplowService(true);
                 //                QLog.l().logQUser().debug("    --> Cust: " + customer.getName() + "; Cust Seq: "
                 //                        + (customer.getIsInSequence() ? "Yes" : "No"));
 
